e5586f5da93ab88109575daa79352c4a78f53770
chrisdelf <chrisdel@protonmial.com>
2023-03-22 05:53:32 +0000

    sfsd

--- src/App.js
+++ src/App.js
@@ -10,16 +10,17 @@ import Login from "./components/login/login.js"
 import Register from "./components/login/register.js"
 import Homepage from "./components/homepage/homepage.js"
 
-
+const audio = new Audio();
 
 function App() {
+  const audioMain = audio
 
   return (
     <Router>
       <Routes>
         <Route exact path="/" element=<Login /> />
         <Route exact path="/register" element=<Register /> />
-        <Route exact path="/homepage" element=<Homepage /> />
+        <Route exact path="/homepage" element=<Homepage audioMain={audioMain}/> />
       </Routes>
     </Router>
   );
